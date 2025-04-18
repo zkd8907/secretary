3786f1494f33c6f2b84d87eb4741466dd093a46a
Bobby <zkd8907@live.com>
2025-04-18 11:02:40 +0800

    feat: 更新Twitter模块与配置管理
    - 在README.md中更新Twitter相关配置，新增用户名和密码环境变量。
    - 在.gitignore中添加会话文件（*.tw_session），避免提交到代码仓库。
    - 修改twitter.py，优化获取Twitter帖子逻辑，支持会话管理和空帖子处理。

--- modules/socialmedia/twitter.py
+++ modules/socialmedia/twitter.py
@@ -1,19 +1,22 @@
+import os
 from tweety import Twitter
 from utils.redisClient import redis_client
 from modules.socialmedia.post import Post
+
 app = Twitter('session')
+app.start(os.getenv('TWITTER_USERNAME'), os.getenv('TWITTER_PASSWORD'))
 
 
 def fetch(user_id: str) -> list[Post]:
-    last_post_id = redis_client.get(f"tweet:{user_id}:last_post_id")
+    cursor = redis_client.get(f"twitter:{user_id}:last_post_id")
 
-    if last_post_id is None:
-        last_post_id = '0'
+    if cursor is None:
+        cursor = ''
     else:
-        last_post_id = str(last_post_id, encoding='utf-8')
+        cursor = str(cursor, encoding='utf-8')
 
     try:
-        posts = app.get_tweets(user_id)
+        posts = app.get_tweets(user_id, cursor=cursor)
     except Exception as e:
         print(e)
         return []
@@ -21,15 +24,25 @@ def fetch(user_id: str) -> list[Post]:
     noneEmptyPosts = []
 
     for post in posts:
-        if post.id > last_post_id:
-            last_post_id = post.id
-        else:
-            continue
+        if 'tweets' in post:
+            latest_id = None
+            latest_created_on = None
+            combined_text = ""
 
-        if post.text:
+            for tweet in post.tweets:
+                if tweet.text:
+                    combined_text += tweet.text + "\n"
+                if latest_created_on is None or tweet.created_on > latest_created_on:
+                    latest_created_on = tweet.created_on
+                    latest_id = tweet.id
+
+            if combined_text and latest_id and latest_created_on:
+                noneEmptyPosts.append(
+                    Post(latest_id, latest_created_on, combined_text.strip()))
+        elif post.text:
             noneEmptyPosts.append(Post(post.id, post.created_on, post.text))
 
-    redis_client.set(f"tweet:{user_id}:last_post_id", last_post_id)
+    redis_client.set(f"twitter:{user_id}:last_post_id", posts.cursor_top)
 
     return noneEmptyPosts
 
